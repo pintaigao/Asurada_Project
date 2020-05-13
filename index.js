fb644d6a4945fbcfb1a16295738a9fbbb8589f78
pintaigao <pintaigao@gmail.com>
2020-05-13 15:23:25 -0500

    sync the project

--- index.js
+++
@@ -1,5 +0,0 @@
-const bluetooth = require("@abandonware/noble");
-bluetooth.startScanning(["A440E3CD-C26B-4EAD-8066-13BE0D3F0EB1"], true, (err) => {
-    console.log("There may be a error");
-    console.log(err);
-});
